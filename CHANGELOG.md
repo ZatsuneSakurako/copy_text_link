c156874c06a5575b765d11ab17db38f22a44f54b
Zatsune Sakurako <ZatsuneSakurako@users.noreply.github.com>
2025-11-15 16:53:17 +0100

    promise version of notification.create

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 # WIP
 * i : Code cleaning
+* i : Use [Promise](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise) return version of [chrome.notifications.create](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/notifications/create)
 * i : Write clipboard using [navigator.clipboard.writeText](https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/writeText)
 
 # 2.1.0
--- webextension/index.js
+++ webextension/index.js
@@ -6,29 +6,14 @@ let _ = chrome.i18n.getMessage;
  *
  * @param {string} title
  * @param {string} message
- * @return {Promise<void>}
+ * @return {Promise<string>}
  */
 function doNotif(title, message) {
-	const options = {
+	return (self.browser ?? chrome).notifications.create({
 		type: "basic",
 		title: title,
 		message: message,
 		contextMessage: chrome.runtime.getManifest().name,
-		iconUrl: "/icon.png",
-		isClickable: true
-	};
-
-	return new Promise((resolve, reject) => {
-		chrome.notifications.create(options, function() {
-			if (typeof chrome.runtime.lastError === "object" && chrome.runtime.lastError !== null && typeof chrome.runtime.lastError.message === "string" && chrome.runtime.lastError.message.length > 0) {
-				const error = chrome.runtime.lastError;
-				// strip-debug-ignore-next
-				console.error(error);
-				reject(error);
-			} else {
-				resolve();
-			}
-		});
 	});
 }
 chrome.notifications.onClicked.addListener(function (notificationId) {
@@ -46,15 +31,14 @@ await chrome.contextMenus.create({
 	targetUrlPatterns: ["http://*/*", "https://*/*"]
 });
 chrome.contextMenus.onClicked.addListener(function (info, tab) {
-	chrome.tabs.sendMessage(tab.id, {
-		id: "copyLinkText",
-		data: ""
-	}, async function (responseData) {
+	chrome.tabs.sendMessage(tab.id, { id: "copyLinkText" }, async function (responseData) {
 		const success = responseData && responseData.error === false;
 		if (!success) {
-			doNotif(_("errorTitle"), _("errorDesc"));
+			doNotif(_("errorTitle"), _("errorDesc"))
+				.then(console.debug)
+				// strip-debug-ignore-next
+				.catch(console.error);
 		}
-
 		console[(success) ? "debug" : "warn"](`Copy to clipboad ${(success) ? "success" : "error"} (${responseData?.error})`);
 	});
 })
